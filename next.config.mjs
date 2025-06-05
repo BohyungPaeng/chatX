5b349db02f2324708b23a22e53a1539ecbdb3f43
BohyungPaeng <sthinnovative@gmail.com>
2025-06-05 16:22:31 +0900

    chore: vercel 버그로인한 배포실패를 해결하기위해, 명시적으로 ES Modules 내 webpack 옵션 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // 추가 for vercel
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -8,6 +9,13 @@ const nextConfig = {
   },
   images: {
     unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': process.cwd(),
+    };
+    return config;
   },
 }
 
